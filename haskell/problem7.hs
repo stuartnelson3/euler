435f6bb4933dde8e4464f2efb3eea0be29f7bcdb
Stuart Nelson <stuartnelson3@gmail.com>
2013-08-12 15:49:29 -0500

    problem7 in haskell

--- haskell/problem7.hs
+++ haskell/problem7.hs
@@ -2,6 +2,4 @@ isqrt :: Integer -> Integer
 isqrt x = floor $ sqrt $ fromIntegral x
 is_prime :: Integer -> Bool
 is_prime k = null [ x | x <- [2..isqrt k], k `mod` x == 0]
-source = 600851475143
-sq = isqrt source
-maximum [ x | x <- [3..sq], is_prime x, source `mod` x == 0]
+main = print $ last $ take 10001 [ x | x <- [2..], is_prime x ]
