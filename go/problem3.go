c6174843c2869debd42c1ab41917a7970b4cc736
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-25 22:48:00 -0400

    problem3 in go

---
+++ go/problem3.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+    "fmt"
+    "math"
+)
+
+func is_prime(n int) bool {
+    for j:=2;j<n;j++{
+        if n%j==0{
+            break
+        } else if j == n-1 {
+            return true
+        }
+    }
+    return false
+}
+
+func main() {
+    target := 600851475143
+    max := int(math.Floor(math.Sqrt(600851475143)))
+    var candidate int
+    for i:=1;i<max;i++{
+        if target%i==0{
+            if is_prime(i) {
+                candidate = i
+            }
+        }
+    }
+    fmt.Println("Winner is:", candidate)
+}
