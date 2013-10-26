7d5ea208b75c5dbbbd30a8335153ffb60638a300
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-25 22:08:19 -0400

    problem2 in go

---
+++ go/problem2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+    if n == 0 || n == 1 {
+        return n
+    } else {
+        return fib(n-1) + fib(n-2)
+    }
+}
+
+func main() {
+    sum := 0
+    for i:=0;i<4000000;i++{
+        x := fib(i)
+        if x > 4000000 { break }
+        if x%2==0 { sum += x }
+    }
+    fmt.Println("Sum is:", sum)
+}
